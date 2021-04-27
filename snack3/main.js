4e6a33920c753f800e03c2927dc298fb58ef8f52
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-04-27 12:01:18 +0200

    aggiunta possibilità di  inserire numeri negativi

--- snack3/main.js
+++ snack3/main.js
@@ -1,11 +1,11 @@
-// Chiedere in input 10 numeri e sommare solo quelli positivi.
+// Chiedere in input 10 numeri e li somma.
 
 var somma = 0;
 var n;
 
 for (var i = 0; i < 10; i++){
   n = parseInt(prompt('Inserisci un numero '));
-  if (n > 0){
+  if (n > 0 || n < 0){
     somma += n;
   }
 }
