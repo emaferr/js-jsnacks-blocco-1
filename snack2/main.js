cfc04803d90b94b44dd12b498510a55ef385b68e
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-04-27 10:53:18 +0200

    End snack 2

---
+++ snack2/main.js
@@ -0,0 +1,11 @@
+var word_1 = prompt("Inserisci una parola");
+var word_2 = prompt("Inserisci un'altra parola");
+
+// Il software stampa il maggiore
+if(word_1.length === word_2.length){
+    console.log("Le parole sono uguali riprova");
+}else if (word_1.length > word_2.length){
+    console.log(word_1);
+}else {
+    console.log(word_2);
+}
