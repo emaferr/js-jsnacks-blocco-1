037fd053b8f8744bc17cc751884853bdae15975b
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-04-28 12:51:21 +0200

    End snack8

---
+++ snack8/main.js
@@ -0,0 +1,16 @@
+// Chiedi un numero di 4 cifre all’utente e 
+
+var numeroUtente = Number(prompt("Inserisci un numero di quattro cifre"));
+
+// calcola la somma di tutte le cifre che compongono il numero.
+
+var num = numeroUtente, sum = 0;
+while ( num > 0 ) { 
+    sum += (num % 10)|0; num /= 10; }
+document.write(sum);
+
+
+
+
+
+
